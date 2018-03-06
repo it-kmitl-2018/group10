e6ebbab8823b85ef76d5bd5831b4508a61755e46
Unknown <kuro-neko@hotmail.co.th>
2018-03-06 11:35:20 +0700

    Edit seller to vendor

--- Backend/src/main/kotlin/th/ac/kmitl/it/soa/group10/model/ShipFromModel.kt
+++ Backend/src/main/kotlin/th/ac/kmitl/it/soa/group10/model/ShipFromModel.kt
@@ -1,9 +1,9 @@
 package th.ac.kmitl.it.soa.group10.model
 
-data class ShipFromModel(var sellerCode: String, var interSellerCode: String, var senderName: String){
+data class ShipFromModel(var vendorCode: String, var interVendorCode: String, var senderName: String){
 
     override fun toString(); String {
-        return "ShipFromModel(sellerCode=$sellerCode, interSellerCode=$interSellerCode, senderName=$senderName)";
+        return "ShipFromModel(vendorCode=$vendorCode, interVendorCode=$interVendorCode, senderName=$senderName)";
     }
 
 }
