b867426fd8ff8d2215dbdb7f366786048df25cce
Unknown <kuro-neko@hotmail.co.th>
2018-03-06 12:50:56 +0700

    Add PostalTradAddressModel

--- Backend/src/main/kotlin/th/ac/kmitl/it/soa/group10/model/supplyChainTradeTransaction/PostalTradeAddressModel.kt
+++ Backend/src/main/kotlin/th/ac/kmitl/it/soa/group10/model/supplyChainTradeTransaction/PostalTradeAddressModel.kt
@@ -2,6 +2,7 @@ package th.ac.kmitl.it.soa.group10.model.supplyChainTradeTransaction
 
 data class PostalTradeAddressModel(var postcodeCode: String, var cityName: String, var citySubDivisionName: String,
                                    var countryId: String, var countrySubDivisionId: String, var buildingNumber: String){
+
     override fun toString(): String {
         return "PostalTradeAddressModel(postcodeCode=$postcodeCode, cityName=$cityName, " +
                 "citySubDivisionName=$citySubDivisionName, countryId=$countryId, " +
