10314aba2e8585bcc96141cb1d0aab087c167bec
Unknown <kuro-neko@hotmail.co.th>
2018-03-06 12:51:41 +0700

    Delete ShipFromModel

--- Backend/src/main/kotlin/th/ac/kmitl/it/soa/group10/model/ShipFromModel.kt
+++
@@ -1,9 +0,0 @@
-package th.ac.kmitl.it.soa.group10.model
-
-data class ShipFromModel(var sellerId: String, var senderName: String){
-
-    override fun toString(); String {
-        return "ShipFromModel(sellerId=$sellerId, senderName=$senderName)";
-    }
-
-}
