36813af13cdfb65f4dc3fc89bbf05e06b4e447e9
aliveable <aliveable1754@gmail.com>
2018-04-28 21:34:05 +0700

    delete unuse file

--- src/main/java/th/ac/kmitl/it/soa/group10/Backend/controller/UserController.java
+++ src/main/java/th/ac/kmitl/it/soa/group10/Backend/controller/UserController.java
@@ -1,17 +1,23 @@
 package th.ac.kmitl.it.soa.group10.Backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import th.ac.kmitl.it.soa.group10.Backend.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
+@RequestMapping("/api")
 public class UserController {
+    String id = "";
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
 
-    //Initialize Example
-    @RequestMapping(value = "/user/1", method = RequestMethod.GET)
-    public User getUser() {
-        return new User(1, "John");
+    public static final String HELLO_TEXT = "Hello from Spring Boot Backend!";
+
+    @RequestMapping(path = "/hello")
+    public @ResponseBody String sayHello() {
+        LOG.info("GET called on /hello resource");
+        return HELLO_TEXT;
     }
 
 }
