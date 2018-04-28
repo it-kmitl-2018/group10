6c49799de345f7980de78d775e713b7d22d06441
aliveable <aliveable1754@gmail.com>
2018-04-28 22:38:01 +0700

    Create PayerTradeFormModel

---
+++ src/main/java/th/ac/kmitl/it/soa/group10/Backend/model/PayerTradePartyFormModel.java
@@ -0,0 +1,166 @@
+package th.ac.kmitl.it.soa.group10.Backend.model;
+
+public class PayerTradePartyFormModel {
+     String payerID;
+     String payerGlobalID;
+     String payerName;
+     String payerTaxID;
+     String payerEmail;
+     String payerBuildingName;
+     String payerBuildingNo;
+     String payerAlley;
+     String payerVillage;
+     String payerVillageNo;
+     String payerStreet;
+     String payerSubDistrict;
+     String payerDistrict;
+     String payerSubCountry;
+     String payerCountryCode;
+     String payerPostCode;
+     String payerAddrLineOne;
+     String payerAddrLineTwo;
+
+    public String getPayerID() {
+        return payerID;
+    }
+
+    public void setPayerID(String payerID) {
+        this.payerID = payerID;
+    }
+
+    public String getPayerGlobalID() {
+        return payerGlobalID;
+    }
+
+    public void setPayerGlobalID(String payerGlobalID) {
+        this.payerGlobalID = payerGlobalID;
+    }
+
+    public String getPayerName() {
+        return payerName;
+    }
+
+    public void setPayerName(String payerName) {
+        this.payerName = payerName;
+    }
+
+    public String getPayerTaxID() {
+        return payerTaxID;
+    }
+
+    public void setPayerTaxID(String payerTaxID) {
+        this.payerTaxID = payerTaxID;
+    }
+
+    public String getPayerEmail() {
+        return payerEmail;
+    }
+
+    public void setPayerEmail(String payerEmail) {
+        this.payerEmail = payerEmail;
+    }
+
+    public String getPayerBuildingName() {
+        return payerBuildingName;
+    }
+
+    public void setPayerBuildingName(String payerBuildingName) {
+        this.payerBuildingName = payerBuildingName;
+    }
+
+    public String getPayerBuildingNo() {
+        return payerBuildingNo;
+    }
+
+    public void setPayerBuildingNo(String payerBuildingNo) {
+        this.payerBuildingNo = payerBuildingNo;
+    }
+
+    public String getPayerAlley() {
+        return payerAlley;
+    }
+
+    public void setPayerAlley(String payerAlley) {
+        this.payerAlley = payerAlley;
+    }
+
+    public String getPayerVillage() {
+        return payerVillage;
+    }
+
+    public void setPayerVillage(String payerVillage) {
+        this.payerVillage = payerVillage;
+    }
+
+    public String getPayerVillageNo() {
+        return payerVillageNo;
+    }
+
+    public void setPayerVillageNo(String payerVillageNo) {
+        this.payerVillageNo = payerVillageNo;
+    }
+
+    public String getPayerStreet() {
+        return payerStreet;
+    }
+
+    public void setPayerStreet(String payerStreet) {
+        this.payerStreet = payerStreet;
+    }
+
+    public String getPayerSubDistrict() {
+        return payerSubDistrict;
+    }
+
+    public void setPayerSubDistrict(String payerSubDistrict) {
+        this.payerSubDistrict = payerSubDistrict;
+    }
+
+    public String getPayerDistrict() {
+        return payerDistrict;
+    }
+
+    public void setPayerDistrict(String payerDistrict) {
+        this.payerDistrict = payerDistrict;
+    }
+
+    public String getPayerSubCountry() {
+        return payerSubCountry;
+    }
+
+    public void setPayerSubCountry(String payerSubCountry) {
+        this.payerSubCountry = payerSubCountry;
+    }
+
+    public String getPayerCountryCode() {
+        return payerCountryCode;
+    }
+
+    public void setPayerCountryCode(String payerCountryCode) {
+        this.payerCountryCode = payerCountryCode;
+    }
+
+    public String getPayerPostCode() {
+        return payerPostCode;
+    }
+
+    public void setPayerPostCode(String payerPostCode) {
+        this.payerPostCode = payerPostCode;
+    }
+
+    public String getPayerAddrLineOne() {
+        return payerAddrLineOne;
+    }
+
+    public void setPayerAddrLineOne(String payerAddrLineOne) {
+        this.payerAddrLineOne = payerAddrLineOne;
+    }
+
+    public String getPayerAddrLineTwo() {
+        return payerAddrLineTwo;
+    }
+
+    public void setPayerAddrLineTwo(String payerAddrLineTwo) {
+        this.payerAddrLineTwo = payerAddrLineTwo;
+    }
+}
