dbe2623903ce6d502c11587160bf16f58c8637b8
aliveable <aliveable1754@gmail.com>
2018-04-28 20:35:26 +0700

    create payeeTradePartyForm model

---
+++ src/main/java/th/ac/kmitl/it/soa/group10/Backend/model/PayeeTradePartyFormModel.java
@@ -0,0 +1,166 @@
+package th.ac.kmitl.it.soa.group10.Backend.model;
+
+public class PayeeTradePartyFormModel {
+    String id;
+    String payerGlobalID;
+    String name;
+    String taxID;
+    String email;
+    String buildingName;
+    String buildingNo;
+    String alley;
+    String village;
+    String villageNo;
+    String street;
+    String subDistrict;
+    String district;
+    String subCountry;
+    String countrycode;
+    String postcode;
+    String addrLineOne;
+    String addrLineTwo;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPayerGlobalID() {
+        return payerGlobalID;
+    }
+
+    public void setPayerGlobalID(String payerGlobalID) {
+        this.payerGlobalID = payerGlobalID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTaxID() {
+        return taxID;
+    }
+
+    public void setTaxID(String taxID) {
+        this.taxID = taxID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public String getBuildingNo() {
+        return buildingNo;
+    }
+
+    public void setBuildingNo(String buildingNo) {
+        this.buildingNo = buildingNo;
+    }
+
+    public String getAlley() {
+        return alley;
+    }
+
+    public void setAlley(String alley) {
+        this.alley = alley;
+    }
+
+    public String getVillage() {
+        return village;
+    }
+
+    public void setVillage(String village) {
+        this.village = village;
+    }
+
+    public String getVillageNo() {
+        return villageNo;
+    }
+
+    public void setVillageNo(String villageNo) {
+        this.villageNo = villageNo;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getSubDistrict() {
+        return subDistrict;
+    }
+
+    public void setSubDistrict(String subDistrict) {
+        this.subDistrict = subDistrict;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getSubCountry() {
+        return subCountry;
+    }
+
+    public void setSubCountry(String subCountry) {
+        this.subCountry = subCountry;
+    }
+
+    public String getCountrycode() {
+        return countrycode;
+    }
+
+    public void setCountrycode(String countrycode) {
+        this.countrycode = countrycode;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getAddrLineOne() {
+        return addrLineOne;
+    }
+
+    public void setAddrLineOne(String addrLineOne) {
+        this.addrLineOne = addrLineOne;
+    }
+
+    public String getAddrLineTwo() {
+        return addrLineTwo;
+    }
+
+    public void setAddrLineTwo(String addrLineTwo) {
+        this.addrLineTwo = addrLineTwo;
+    }
+}
