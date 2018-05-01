4a43ab6541231c5495655c170ab9cf07817d8a1a
Pattiiz <phat.my.name@gmail.com>
2018-05-02 01:27:42 +0700

    Rename test to SellerFormTests
    Rename Seller"Model"Tests to Seller"Form"Tests

--- src/test/java/th/ac/kmitl/it/soa/group10/Backend/SellerFormTests.java
+++ src/test/java/th/ac/kmitl/it/soa/group10/Backend/SellerFormTests.java
@@ -1,115 +1,113 @@
 package th.ac.kmitl.it.soa.group10.Backend;
 
 import org.junit.Test;
-import th.ac.kmitl.it.soa.group10.Backend.model.SellerModel;
-
-import java.lang.annotation.Target;
+import th.ac.kmitl.it.soa.group10.Backend.model.SellerForm;
 
 import static org.junit.Assert.assertEquals;
 
-public class SellerModelTests {
+public class SellerFormTests {
 
     @Test
     public void testID() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setId("0001");
         assertEquals("0001", seller.getId());
     }
 
     @Test
     public void testName() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setName("John");
         assertEquals("John", seller.getName());
     }
 
     @Test
     public void testEmail() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setEmail("John@gmail.com");
         assertEquals("John@gmail.com", seller.getEmail());
     }
 
     @Test
     public void testBuildingName() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setBuildingName("Gaysorn Plaza");
         assertEquals("Gaysorn Plaza", seller.getBuildingName());
     }
 
     @Test
     public void testBuildingNo() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setBuildingNo("58/2");
         assertEquals("58/2", seller.getBuildingNo());
     }
 
     @Test
     public void testVillage() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setVillage("-");
         assertEquals("-", seller.getVillage());
     }
 
     @Test
     public void testVillageNo() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setVillageNo("-");
         assertEquals("-", seller.getVillageNo());
     }
 
     @Test
     public void testStreet() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setStreet("henri dunant");
         assertEquals("henri dunant", seller.getStreet());
     }
 
     @Test
     public void testSubDistrict() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setSubDistrict("Lumphini");
         assertEquals("Lumphini", seller.getSubDistrict());
     }
 
     @Test
     public void testDistrict() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setDistrict("Patum-one");
         assertEquals("Patum-one", seller.getDistrict());
     }
 
     @Test
     public void testSubCountry() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setSubCountry("Bangkok");
         assertEquals("Bangkok", seller.getSubCountry());
     }
 
     @Test
     public void testCountry() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setCountry("Thailand");
         assertEquals("Thailand", seller.getCountry());
     }
 
     @Test
     public void testPostCode() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setPostCode("10330");
         assertEquals("10330", seller.getPostCode());
     }
 
     @Test
     public void testAddrLineOne() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setAddrLineOne("309 หมู่ 10 ถนนฉลองกรุง");
         assertEquals("309 หมู่ 10 ถนนฉลองกรุง", seller.getAddrLineOne());
     }
 
     @Test
     public void testAddrLineTwo() {
-        SellerModel seller = new SellerModel();
+        SellerForm seller = new SellerForm();
         seller.setAddrLineTwo("458 หมู่ 3 ถนนรามอินทรา");
         assertEquals("458 หมู่ 3 ถนนรามอินทรา", seller.getAddrLineTwo());
     }
