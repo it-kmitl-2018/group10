bde763604760e500cf12ac28f4351dfe4fe5551b
Pattiiz <phat.my.name@gmail.com>
2018-05-02 01:23:06 +0700

    Rename ConsigneeModelTest to ConsigneeFormTest

--- src/test/java/th/ac/kmitl/it/soa/group10/Backend/ConsigneeFormTests.java
+++ src/test/java/th/ac/kmitl/it/soa/group10/Backend/ConsigneeFormTests.java
@@ -1,114 +1,114 @@
 package th.ac.kmitl.it.soa.group10.Backend;
 
 import org.junit.Test;
-import th.ac.kmitl.it.soa.group10.Backend.model.ConsigneeModel;
+import th.ac.kmitl.it.soa.group10.Backend.model.ConsigneeForm;
 
 import static org.junit.Assert.assertEquals;
 
-public class ConsigneeModelTests {
+public class ConsigneeFormTests {
 
 
     @Test
     public void testID() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setId("0001");
         assertEquals("0001", consignee.getId());
     }
 
     @Test
     public void testName() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setName("John");
         assertEquals("John", consignee.getName());
     }
 
     @Test
     public void testEmail() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setEmail("John@gmail.com");
         assertEquals("John@gmail.com", consignee.getEmail());
     }
 
     @Test
     public void testBuildingName() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setBuildingName("Gaysorn Plaza");
         assertEquals("Gaysorn Plaza", consignee.getBuildingName());
     }
 
     @Test
     public void testBuildingNo() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setBuildingNo("58/2");
         assertEquals("58/2", consignee.getBuildingNo());
     }
 
     @Test
     public void testVillage() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setVillage("-");
         assertEquals("-", consignee.getVillage());
     }
 
     @Test
     public void testVillageNo() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setVillageNo("-");
         assertEquals("-", consignee.getVillageNo());
     }
 
     @Test
     public void testStreet() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setStreet("henri dunant");
         assertEquals("henri dunant", consignee.getStreet());
     }
 
     @Test
     public void testSubDistrict() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setSubDistrict("Lumphini");
         assertEquals("Lumphini", consignee.getSubDistrict());
     }
 
     @Test
     public void testDistrict() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setDistrict("Patum-one");
         assertEquals("Patum-one", consignee.getDistrict());
     }
 
     @Test
     public void testSubCountry() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setSubCountry("Bangkok");
         assertEquals("Bangkok", consignee.getSubCountry());
     }
 
     @Test
     public void testCountry() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setCountry("Thailand");
         assertEquals("Thailand", consignee.getCountry());
     }
 
     @Test
     public void testPostCode() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setPostCode("10330");
         assertEquals("10330", consignee.getPostCode());
     }
 
     @Test
     public void testAddrLineOne() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setAddrLineOne("309 หมู่ 10 ถนนฉลองกรุง");
         assertEquals("309 หมู่ 10 ถนนฉลองกรุง", consignee.getAddrLineOne());
     }
 
     @Test
     public void testAddrLineTwo() {
-        ConsigneeModel consignee = new ConsigneeModel();
+        ConsigneeForm consignee = new ConsigneeForm();
         consignee.setAddrLineTwo("458 หมู่ 3 ถนนรามอินทรา");
         assertEquals("458 หมู่ 3 ถนนรามอินทรา", consignee.getAddrLineTwo());
     }
