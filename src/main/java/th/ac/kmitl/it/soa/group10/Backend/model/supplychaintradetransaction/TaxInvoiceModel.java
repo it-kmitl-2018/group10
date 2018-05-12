5a8590669a9550f8312aed5eae571e1748ebfff2
Unknown <kuro-neko@hotmail.co.th>
2018-05-12 23:49:56 +0700

    Create TaxInvoiceModel and Test

---
+++ src/main/java/th/ac/kmitl/it/soa/group10/Backend/model/supplychaintradetransaction/TaxInvoiceModel.java
@@ -0,0 +1,40 @@
+package th.ac.kmitl.it.soa.group10.Backend.model.supplychaintradetransaction;
+
+public class TaxInvoiceModel {
+    private String taxCode;
+    private String taxRate;
+    private String valueAmount;
+    private String vatAmount;
+
+    public String getTaxCode() {
+        return taxCode;
+    }
+
+    public void setTaxCode(String taxCode) {
+        this.taxCode = taxCode;
+    }
+
+    public String getTaxRate() {
+        return taxRate;
+    }
+
+    public void setTaxRate(String taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public String getValueAmount() {
+        return valueAmount;
+    }
+
+    public void setValueAmount(String valueAmount) {
+        this.valueAmount = valueAmount;
+    }
+
+    public String getVatAmount() {
+        return vatAmount;
+    }
+
+    public void setVatAmount(String vatAmount) {
+        this.vatAmount = vatAmount;
+    }
+}
