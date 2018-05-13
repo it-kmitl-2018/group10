e6c9e680f4cd3a3cb720611de1adab30af6f74e4
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 11:36:08 +0700

    Fix build
    แก้ไข path import1 ผิด

--- src/test/java/th/ac/kmitl/it/soa/group10/Backend/InvoiceTradePartyModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group10/Backend/InvoiceTradePartyModelTest.java
@@ -1,7 +1,7 @@
 package th.ac.kmitl.it.soa.group10.backend;
 
 import org.junit.Test;
-import th.ac.kmitl.it.soa.group10.Backend.model.supplychaintradetransaction.InvoiceTradePartyModel;
+import th.ac.kmitl.it.soa.group10.backend.model.supplychaintradetransaction.InvoiceTradePartyModel;
 
 import static org.junit.Assert.assertEquals;
 
