aa12edbb3f101f03944f80f62f31967a57985a23
Unknown <kuro-neko@hotmail.co.th>
2018-05-13 10:13:18 +0700

    Rename package Backend to  backend

--- src/test/java/th/ac/kmitl/it/soa/group10/Backend/PostalTradeAdressModelTest.java
+++ src/test/java/th/ac/kmitl/it/soa/group10/Backend/PostalTradeAdressModelTest.java
@@ -1,7 +1,7 @@
-package th.ac.kmitl.it.soa.group10.Backend;
+package th.ac.kmitl.it.soa.group10.backend;
 
 import org.junit.Test;
-import th.ac.kmitl.it.soa.group10.Backend.model.supplychaintradetransaction.PostalTradeAddressModel;
+import th.ac.kmitl.it.soa.group10.backend.model.supplychaintradetransaction.PostalTradeAddressModel;
 
 import static org.junit.Assert.assertEquals;
 
